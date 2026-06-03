3c429c974aa7da5dbc6dcf59a7e127e174af5b88
mohitrana9137-arch <mohitrana9137@gmail.com>
2026-06-03 17:32:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class Config:
         self.SESSION3 = getenv("SESSION3", None)
 
         self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Egoistic_You")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+1hA_faGyu85iY2Rl")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+Y-YDCBabr98zMDU9")
 
         self.AUTO_END: bool = getenv("AUTO_END", False)
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
