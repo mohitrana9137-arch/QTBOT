a178a7e5c82e72353e428b3c95b8c367c9781dce
mohitrana9137-arch <mohitrana9137@gmail.com>
2026-06-03 16:35:56 +0530

    Update support channel and chat URLs in config.py

--- config.py
+++ config.py
@@ -19,8 +19,8 @@ class Config:
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AloneUpdates")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AloneBotSupport")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Egoistic_You")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+1hA_faGyu85iY2Rl")
 
         self.AUTO_END: bool = getenv("AUTO_END", False)
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
@@ -35,8 +35,8 @@ class Config:
             if url and "batbin.me" in url
         ]
         self.DEFAULT_THUMB = getenv("DEFAULT_THUMB", "https://te.legra.ph/file/3e40a408286d4eda24191.jpg")
-        self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/haagg2.png")
-        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/zvziwk.jpg")
+        self.PING_IMG = getenv("PING_IMG", "https://files.catbox.moe/br7alp.jpg")
+        self.START_IMG = getenv("START_IMG", "https://files.catbox.moe/br7alp.jpg")
 
     def check(self):
         missing = [
